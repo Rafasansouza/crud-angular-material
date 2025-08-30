2112b30b461023f1fdbd26db8452e43b7373cecb
Rafael Souza <rafasansou1@gmail.com>
2025-08-30 17:32:25 -0300

    feat: adiconar tabela de funcionarios cadastrados

--- src/app/consulta/consulta.ts
+++ src/app/consulta/consulta.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { ClienteService } from '../cliente.service';
+import { Cliente } from '../cadastro/cliente';
 
 @Component({
   selector: 'app-consulta',
-  imports: [],
+  imports: [
+    MatInputModule,
+    MatCardModule,
+    MatIconModule,
+    MatTableModule,
+    MatButtonModule,
+    FormsModule,
+    FlexLayoutModule,
+    CommonModule
+],
   templateUrl: './consulta.html',
   styleUrl: './consulta.scss'
 })
-export class Consulta {
+export class Consulta implements OnInit {
+
+  listaClientes: Cliente[] = [];
+  colunasTabela: string[] = ["id", "nome", "cpf", "dtNascimento", "email"]
+
+  constructor(private service: ClienteService){
+
+  }
+
+  ngOnInit(){
+    this.listaClientes = this.service.consultarCliente('');
+  }
 
 }
